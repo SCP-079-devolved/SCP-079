dc21062943499091b4910d990bb1ee37c259bc0c
Z <78899706+Apraxed@users.noreply.github.com>
2021-10-17 18:49:39 +0000

    Added a few commands and images for a few

---
+++ README.md
@@ -0,0 +1,35 @@
+# SCP-079
+
+SCP-079 is a fully customizable bot created by [Apraxed](https://github.com/Apraxed) and [Carnoval15](https://github.com/Carnoval15), the bot is based off of SCP-079 created by [TuneCode](https://discord.gg/gQxnsdqScw) with the owner's consent to use the name and icon, all material in this bot is copyrighted under a `BSD 3-Clause License` listed under the name "Apraxed" for the owner of this bot, the website is under a `CreativeCommons Attribution-NonCommercial-NoDerivatives 4.0 International` license.
+
+Now on to the bot itself
+
+## Code:
+The bot's code is custom code with command ideas based off of TuneCode's bot SCP-079 but released freely and under the licenses listed above,
+
+### Current commands are:
+
+- Ping (latency)
+- pp_size
+- ban
+- kick
+
+### Commands underway are:
+
+- Mute
+- Warn
+- Meme
+- Luke (creator of original bot's embarresing picture)
+- Sneaky (creator's friend embarresing picture)
+- Sniper (another of the creator's friend embarresing picture)
+- December (same as above description)
+- Report
+
+### Fetures planned:
+
+- Modmail
+- customizable through an attached webpage to the bot
+- Music Functionality (halted due to the discord music bot takedowns)
+- Support discord server
+
+
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import disnake, os, random
 from disnake.ext import commands
-from config.cfg import general, messages
+from config.cfg import general, messages, publiccommands
+
 
 discord = disnake
 
 scp079=commands.Bot(command_prefix=general.prefix, descripion = "Luke did the dumb and yeeted the bot, now I'm here!", test_guilds=[816430534757580830], help_command=None)
 client = scp079
 
+
 @client.slash_command()
 async def ping(ctx):
     e=discord.Embed(title="ping", description=(f"Pong\n\n\n\nPing={round(client.latency)* 1000}ms."))
     await ctx.response.send_message(embed=e)
+
+@client.slash_command()
+async def pp_size(ctx):
+    embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (random.randint(1, 500)), color = 0x5867fe)
+    await ctx.send(embed = embed)
+
+@client.slash_command()
+@commands.has_permission(ban_members=True)
+async def ban(ctx, member: discord.Member, reason = None):
+      if reason == None:
+           reason = messages.ban_message
+      author = ctx.author
+      authorid = ctx.author.id
+      await member.send(f'You have been banned from {ctx.guild.name} for {reason} by {author}')
+      await ctx.guild.ban(member, reason = reason)
+      await ctx.channel.send(f'{member} has been banned for {reason} by <@{authorid}> ')
+
+@client.slash_command()
+@commands.has_permission(kick_members=True)
+async def kick(ctx, member: discord.Member, reason = None):
+    if reason == None:
+      reason = messages.kick_message
+    author = ctx.author
+    authorid = ctx.author.id
+    invitelink = await ctx.channel.create_invite(max_uses=1,unique=True)
+    server = ctx.guild
+    await member.send(f'{author} has kicked you from {server} for {reason}. rejoin with this link {invitelink}')
+    await ctx.send(f'Kicked {member} for you <@{authorid}>, they have recived another invite link but they have hopefully learned')
+    await ctx.guild.kick(member, reason = reason)
 
 token = (os.getenv('Token'))
