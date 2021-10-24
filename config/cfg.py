e4e8af4e4c91e0d58ec254f2387ff7d3728e03f2
Z <78899706+Apraxed@users.noreply.github.com>
2021-10-23 17:37:04 -0700

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -1,28 +1,31 @@
 import os
 
-token=os.getenv("Token")
-f=False
-F=f
-t=True
-T=t
-Default = "sweptbux"
+class config():
+    f=False
+    F=f
+    t=True
+    T=t
 
 class general():
+    token=os.getenv("Token")
     bot_icon_emoji = "<:079:900247468845977610>"
     # Is the ban appealable? T or F
-    ban_appealable=f
+    ban_appealable=config.f # just change the f to true if it is configurable then go to the `if ban_appealable` if statement
+    default_currency_name = "sweptbux" # set your own currency name in banksystem class using this format: "{NAME}"
     # Bot's prefix when not using slash commands
     prefix="."
     # only fill out of ban_appealable = T or t
     if ban_appealable:
         appeals_server="discord.gg/CODEHERE"
+    elif ban_appealable == False:
+        appeals_server="This is an unappealable ban"
 
 class messages():
     ban_message = f'You have been banned from the server'
     kick_message = f'You have been kicked from the server'
 
 class banksystem():
-    name = Default
+    name = general.default_currency_name
     # Emoji embed code for the emoji
     icon = "<:Sweptbux:900246927847866428>"
 
