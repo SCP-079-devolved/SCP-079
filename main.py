c6659237d872c4cb521ebb2f56659a4a15c025d4
Z <78899706+Apraxed@users.noreply.github.com>
2021-11-09 20:39:46 -0800

    Added images commands that actually work

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from server import runserver
 runserver()
 discord = disnake
 
-scp079=commands.Bot(command_prefix=commands.when_mentioned_or(general.prefix), descripion = "Luke did the dumb and yeeted the bot, now I'm here!", test_guilds=[899374265512624138], help_command=None, intents=discord.Intents.all(), case_insensitive=True)
+scp079=commands.Bot(command_prefix=commands.when_mentioned_or(general.prefix), descripion = "Luke did the dumb and yeeted the bot, now I'm here!", test_guilds=[899374265512624138, 905153470993158174], help_command=None, intents=discord.Intents.all(), case_insensitive=True)
 client = scp079
 
 @scp079.event
@@ -16,35 +16,57 @@ async def on_ready():
 
 @client.slash_command()
 async def pp_width(ctx):
+    '''Your pp width....'''
     if ctx.author.id == 756147569880727627:
         embed = discord.Embed(title = f"{ctx.author} your pp width is...", description = (f"1 inch u noob"), color = 0x5867f2)
     else:    
         embed = discord.Embed(title = f"{ctx.author} your pp width is...", description = (f"{random.randint(1, 10)} inches"), color = 0x5867f2)
     await ctx.response.send_message(embed = embed)
+    print(f'command pp_width run in {ctx.guild}')
 
-@client.slash_command()
+@client.command()
 async def comedy(ctx):
-    await ctx.send(file=discord.File("images/comedy.png"))
+    '''Some comedy pic'''
+    author = ctx.author
+    await author.send("https://cdn.discordapp.com/attachments/903454233037254666/904242252434505748/comedy.png")
+    print(f'command comedy run in {ctx.guild}')
 
 @client.slash_command()
 async def ping(ctx):
+    '''ping ...'''
     ping = int(round(client.latency, 3) * 1000)
     e=discord.Embed(title="ping", description=(f"Pong\n\nPing={ping}ms."))
     await ctx.response.send_message(embed=e)
 
 @client.slash_command()
 async def pp_size(ctx):
+    '''Your pp size...'''
+    channel = client.get_channel(899378836968439878)
     if ctx.author.id == 756147569880727627:
-        embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (f"{random.randint(1,8)} inch u noob"), color = 0x5867f2)
-    elif ctx.author.id == 441032877992574986:
-        embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (f"{random.randint(400,500)} inch"), color = 0x5867f2)
+        embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (f"{random.randint(1,2)} inch u noob"), color = 0x5867f2)
+
     else:
-        embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (f"{random.randint(1, 500)} inches"), color = 0x5867f2)
+        size = random.randint(1, 500)
+        print(size)
+        if size == 152:
+            yes=random.randint(500, 501)
+            if yes == 501:
+                embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = ("501 inches, wait something ain't right here"), color = 0x5867f2)
+                print(ctx.author, "GOT 501 INCHES")
+                channel.send(f'ATTENTION @everyone, user <@{ctx.author.id}> got the RAREST dick size, 501, 501 inches is impossible but they still got it, congrats! ')
+            else:
+                embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = ("500 inches"), color = 0x5867f2)
+                
+            
+        else:
+            embed = discord.Embed(title = f"{ctx.author} your pp size is...", description = (f"{size} inches"), color = 0x5867f2)
     await ctx.response.send_message(embed = embed)
+    print(f'command pp_size run in {ctx.guild}')
 
 @client.slash_command()
 @commands.has_any_role('Dev', 'Head Mod', 'Mod')
 async def ban(ctx, member: discord.Member,* ,reason = None):
+    '''To Ban anyone you want in the server! (MOD command)'''
     if member == ctx.author:
         ctx.respoonse.send_message('no banning yourself')    
     else:
@@ -55,10 +77,12 @@ async def ban(ctx, member: discord.Member,* ,reason = None):
         await member.send(f'You have been banned from {ctx.guild.name} for {reason} by {author}')
         await ctx.guild.ban(member, reason = reason)
         await ctx.channel.send(f'{member} has been banned for {reason} by <@{authorid}> ')
+    print(f'ban command run in {ctx.guild}')
 
 @client.slash_command()
 @commands.has_any_role('Dev', 'Head Mod', 'Mod')
 async def kick(ctx, member: discord.Member,* ,reason = None):
+    '''To Kick anyone you want in the server! (MOD command)'''
     if member == ctx.author:
         await ctx.response.send_message('no kicking yourself noob')
         pass
@@ -72,45 +96,49 @@ async def kick(ctx, member: discord.Member,* ,reason = None):
         await member.send(f'{author} has kicked you from {server} for {reason}. rejoin with this link {invitelink}')
         await ctx.response.send_message(f'Kicked {member} for you <@{authorid}>, they have recived another invite link but they have hopefully learned')
         await ctx.guild.kick(member, reason = reason)
+    print(f'kick command run in {ctx.guild}')
 
-class Confirm(disnake.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value = None
+@client.command()
+async def redacted(ctx):
+    await ctx.author.send('https://cdn.discordapp.com/attachments/903454233037254666/906806501061046302/unknown.png')
 
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
-    @disnake.ui.button(label='Shut down', style=disnake.ButtonStyle.red)
-    async def confirm(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-        await interaction.response.send_message('Pls do secret command or type `.cancel`')
-        @client.command()
-        async def shut(ctx, *, reason):
-          await ctx.send("Shutting down...")
-          print(f'{client.user} has shutted down by {ctx.author} because {reason}')
-          exit()
-        
-        @client.command()
-        async def cancel(ctx):
-          await ctx.send("Shut down process canceled.")
-          return
-        self.value = True
-        self.stop()
+@client.command()
+async def aaron(ctx):
+    author = ctx.author
+    await author.send('you owe me ∞ money UwU')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/906049968312311848/Screenshot_20210928-230318_Discord.png')
 
-    # This one is similar to the confirmation button except sets the inner value to `False`
-    @disnake.ui.button(label='Cancel', style=disnake.ButtonStyle.green)
-    async def cancel(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-        await interaction.response.send_message('Cancelling')
-        self.value = False
-        self.stop()
+@client.command()
+async def luke(ctx):
+    author = ctx.author
+    await author.send('UwU you t-touchy my tail')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/904242228803829770/luke.png')
 
+@client.command()
+async def sniper(ctx):
+    author = ctx.author
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/904242035110846504/sniper.jpg')
 
+@client.command()
+async def sneaky(ctx):
+    author = ctx.author
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/904241982870810635/sneaky1.png')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/904241944014753792/sneaky2.png')
+
+@client.command()
+async def december(ctx):
+    author = ctx.author
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/903454303887433808/MirMfPscvzz9Gp1v9Ht2AsHvjZrPx903zOXAXltYiXlTGg4sq5k8KXsmqpyjPX34tbUOG5PT6GisBCtDRrU19YiNSUVAceCFK8hEAgEgneXNyJWAoFAIBC8CkKsBAKBQLDmEWIlEAgEgjWPECuBQCAQrHmEWAkEAoFgzSPESiAQCARrHiFWAoFAIFjzCLESCAQCwZpHiJVAIBAI1jxCrAQCgUCw5hFiJRAIBII1jxArgUAgEKx5hFgJBAKBYM0jxEogEAgEax4hVgKBQCBY8wixEggEAsEa5yjP4liNfAZxcP4AAAAAElFTkSuQmCC.png')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/903454373043109928/IWSvBzO7ZAAAAABJRU5ErkJggg.png')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/903454391498076170/BzGf2butOoxyAAAAAElFTkSuQmCC.png')
+    await author.send('https://cdn.discordapp.com/attachments/903454233037254666/903454410900930570/x8XIKFENCaRlwAAAABJRU5ErkJggg.png')
 
 @client.slash_command()
 async def bug_report(ctx, title, *, description):
-  '''Use this to report a bug! '''
-  print(f'A bug report from {ctx.author}:\n Title:\n {title} \n Description: \n {description}\n ')
-  await ctx.response.send_message(f'Bug has been reported! Thanks 😉')
+    '''Use this to report a bug! '''
+    print(f'A bug report from {ctx.author}:\n Title:\n {title} \n Description: \n {description}\n ')
+    await ctx.response.send_message(f'Bug has been reported! Thanks 😉')
+    print(f'bug report from {ctx.guild}')
 
 token = general.token
 client.run(token)
