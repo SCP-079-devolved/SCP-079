91c3e32cf4d002b7683ff8b8bcc7f4d98cb975ec
Z <78899706+Apraxed@users.noreply.github.com>
2021-11-17 14:14:54 -0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -7,7 +7,7 @@ class config():
     T=t
 
 class general():
-    MOTD = 'Febuary 2022 | We support everyone!'
+    MOTD = 'Now 100% More Slash Commands! | Febuary 2022'
     token=os.getenv("Token")
     bot_icon_emoji = "<:079:900247468845977610>"
     # Is the ban appealable? T or F
@@ -22,11 +22,21 @@ class general():
         appeals_server="This is an unappealable ban"
 
 class messages():
-    ban_message = f'You have been banned from the server'
+    #default reasons for the stuff listed below
+    ban_message = f'You have broken the rules'
     kick_message = f'You have been kicked from the server'
 
 class banksystem():
+    # name of the currency
     name = general.default_currency_name
     # Emoji embed code for the emoji
     icon = "<:Sweptbux:900246927847866428>"
 
+
+class ppsize():
+    mn = 1
+    mx = 500
+    rare = 501
+    unit = "inches"
+    logs = "ppsize.txt"
+    
